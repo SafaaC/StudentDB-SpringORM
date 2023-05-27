4cb2aea817da082191a2c7d0928736bffed8cd3c
SafaaC <safaaacherak@gmail.com>
2023-05-27 12:04:42 +0530

    CRUD Operations Updated

--- src/main/java/com/springorm/dao/StudentDao.java
+++ src/main/java/com/springorm/dao/StudentDao.java
@@ -1,5 +1,7 @@
 package com.springorm.dao;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.orm.hibernate5.HibernateTemplate;
@@ -23,6 +25,34 @@ public class StudentDao {
 	public int insert(Student std) {
 		
 		Integer i=(Integer) this.hibernateTemplate.save(std);
+		System.out.println("data inserted successfully");
 		return i;
 	}
+	
+	//get single data/object
+	public Student getStudent(int stdId) {
+		Student std =hibernateTemplate.get(Student.class, getClass());
+		return std;
+	}
+	
+	//get all students/all row
+	public List<Student> getallStudents(){
+		List<Student> stds=hibernateTemplate.loadAll(Student.class);
+		return stds;
+	}
+	
+	//deleting the data
+	@Transactional
+	public void deleteStudent(int stdId) {
+		Student student=hibernateTemplate.get(Student.class, stdId);
+		hibernateTemplate.delete(student);
+		System.out.println("Student data deleted successfully");
+	}
+	
+	//update data
+	@Transactional
+	public void updateStudent(Student std) {
+		hibernateTemplate.update(std);
+	}
+	
 }
